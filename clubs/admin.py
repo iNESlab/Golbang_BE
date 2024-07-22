8ac40c8dda0e1bb69925b88b2423858918696ed8
Mingguriguri <rlaalswjd-@naver.com>
2024-07-23 02:50:45 +0900

    #14-feat(club): 모임 애플리케이션을 django admin 페이지에 추가

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,3 +1,21 @@
+'''
+MVP demo ver 0.0.1
+2024.07.23
+clubs/admin.py
+'''
 from django.contrib import admin
+from clubs.models import Club
 
-# Register your models here.
+'''
+목록 보기: list_display
+필터: list_filter
+검색: search_fields
+정렬: ordering
+'''
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    list_filter = ('admins', 'created_at')
+    search_fields = ('name', 'members', 'admins')
+    ordering = ('name', 'created_at',)
