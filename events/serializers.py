d2edba7e01f8ae5d7fb9527b87e1fc2886fe7daf
loading1031 <tjdans1031@gmail.com>
2024-07-23 01:30:10 +0900

    Feat: participant 를 상태에 따라 인원수 카운트

--- events/serializers.py
+++ events/serializers.py
@@ -1,5 +1,6 @@
 # events/serializers.py
 from django.db import transaction
+from django.db.models import Q
 from rest_framework import serializers
 
 from clubMembers.models import ClubMember
@@ -16,6 +17,7 @@ class EventCreateSerializer(serializers.ModelSerializer):
         required=True,
         source='club_member'
     )
+
     # request json으로 club_member_id를 받고 이를 ClubMember의 필드인 club_member와 매핑한다.
 
     class Meta:
@@ -50,8 +52,29 @@ class EventDetailSerializer(serializers.ModelSerializer):
         required=True,
         source='club_member'
     )
-
+    participants_count = serializers.SerializerMethodField(read_only=True)
+    party_count = serializers.SerializerMethodField(read_only=True)
+    accept_count = serializers.SerializerMethodField(read_only=True)
+    deny_count = serializers.SerializerMethodField(read_only=True)
+    pending_count = serializers.SerializerMethodField(read_only=True)
+    #TODO: 토큰에서 member_Id 추출하고, 이벤트에서 속한 조, 같은 조 인원을 반환하는 필드 2개 추가해야함
     class Meta:
         model = Event
-        fields = ['event_id', 'club_member_id', 'participants', 'event_title', 'location',
-                  'start_date_time', 'end_date_time', 'repeat_type', 'game_mode', 'alert_date_time']
+        fields = ['event_id', 'club_member_id', 'participants', 'participants_count', 'party_count','accept_count',
+                  'deny_count', 'pending_count', 'event_title', 'location', 'start_date_time', 'end_date_time',
+                  'repeat_type', 'game_mode', 'alert_date_time', ]
+
+    def get_participants_count(self, obj):
+        return obj.participant_set.count()
+    def get_party_count(self, obj):
+        return obj.participant_set.filter(status_type="PARTY").count()
+    def get_accept_count(self, obj):
+        return obj.participant_set.filter(Q(status_type="ACCEPT") | Q(status_type="PARTY")).count()
+    def get_deny_count(self, obj):
+        return obj.participant_set.filter(status_type="DENY").count()
+    def get_pending_count(self, obj):
+        return obj.participant_set.filter(status_type="PENDING").count()
+
+class EventListSerializer(serializers.ModelSerializer):
+    event_list = EventDetailSerializer(source='event_set', many=True)
+
