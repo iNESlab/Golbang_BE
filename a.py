a5b598502eb589b33537e4303fc3290106a3838e
Mingguriguri <rlaalswjd-@naver.com>
2024-08-01 19:43:47 +0900

    feat: pre-commit, conventional commit 테스트

---
+++ a.py
@@ -0,0 +1,31 @@
+x = {"a": 37, "b": 42, "c": 927}
+
+x = 123456789.123456789e123456789
+
+
+class Foo(object):
+    def f(self):
+        return 37 * -2
+
+    def g(self, x, y=42):
+        return y
+
+
+# fmt: off
+custom_formatting = [
+    0,  1,  2,
+    3,  4,  5,
+    6,  7,  8,
+]
+# fmt: on
+regular_formatting = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+]
