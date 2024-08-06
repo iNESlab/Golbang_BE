ab9834cf04a69d7c112dcc339026ab486638afeb
Mingguriguri <rlaalswjd-@naver.com>
2024-08-06 22:51:26 +0900

    fix: ci 파이프라인 내 테스트 실패 부분 관련하여 설정파일 수정

--- golbang/settings/base.py
+++ golbang/settings/base.py
@@ -6,8 +6,8 @@ import os
 import environ
 from pathlib import Path
 # 로컬로 실행이 안 될 시, 아래 두 줄 주석 해제 (단, 깃허브에 올릴 때는 무조건 주석 처리)
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
