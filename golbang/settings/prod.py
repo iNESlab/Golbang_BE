8ede1ad859d0ed9493860da4e29a4cfe59d07af9
Mingguriguri <rlaalswjd-@naver.com>
2024-08-06 19:43:32 +0900

    fix: django 설정 및 배포 환경을 수정된 EC2인스턴스와 RDS에 맞게 수정

--- golbang/settings/prod.py
+++ golbang/settings/prod.py
@@ -11,19 +11,19 @@ environ.Env.read_env(os.path.join(BASE_DIR, "../.env.prod"))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": env("MYSQL_DB_NAME"),
-        "USER": env("MYSQL_DB_USER"),
-        "PASSWORD": env("MYSQL_DB_PASSWORD"),
-        "HOST": env("MYSQL_DB_HOST"),
-        "PORT": env("MYSQL_DB_PORT"),
+        "NAME": env("RDS_MYSQL_DB_NAME"),
+        "USER": env("RDS_MYSQL_DB_USER"),
+        "PASSWORD": env("RDS_MYSQL_DB_PASSWORD"),
+        "HOST": env("RDS_MYSQL_DB_HOST"),
+        "PORT": env("RDS_MYSQL_DB_PORT"),
     }
 }
 
 DEBUG = False
 ALLOWED_HOSTS = [
     env("MAIN_DOMAIN"),
-    "43.202.23.152",
-    # "web",  # Docker Compose 서비스 이름
+    "43.200.41.240",
+    "web",  # Docker Compose 서비스 이름
     # "localhost",
     # "127.0.0.1"
 ]
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = [
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     "http://" + env("MAIN_DOMAIN"),
-    "http://43.202.23.152",
-    "http://localhost:8000",  # Docker 내부에서 접근할 수 있는 도메인
-    "http://127.0.0.1:8000",
+    "http://43.200.41.240",
 ]  # 프로덕션 도메인만 허용
 
 SECURE_SSL_REDIRECT = True  # SSL 사용 시 리디렉션
