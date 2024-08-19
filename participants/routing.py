79a0164e41c0d87f5bf7a1a1b2dd8abe2377284d
loading1031 <tjdans1031@gmail.com>
2024-08-19 14:27:14 +0900

    Fix: routing경로 수정

--- participants/routing.py
+++ participants/routing.py
@@ -3,6 +3,6 @@ from django.urls import path
 from participants.stroke import stroke_event_consumers, stroke_group_consumers
 
 websocket_urlpatterns = [
-    path('ws/stroke/participants/<int:participant_id>/event/stroke', stroke_event_consumers.EventParticipantConsumer.as_asgi()),
-    path('ws/stroke/participants/<int:participant_id>/group/stroke', stroke_group_consumers.GroupParticipantConsumer.as_asgi()),
+    path('ws/participants/<int:participant_id>/event/stroke', stroke_event_consumers.EventParticipantConsumer.as_asgi()),
+    path('ws/participants/<int:participant_id>/group/stroke', stroke_group_consumers.GroupParticipantConsumer.as_asgi()),
 ]
