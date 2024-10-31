3518160b9431226ca17f1dff430805b5bea03002
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2024-10-31 18:17:51 +0900

    #59-feat: Excel 파일을 하나만 유지하고, 새로운 파일이 업로드될 때 기존 파일을 삭제하는 기능 추가

--- golf_data/admin.py
+++ golf_data/admin.py
@@ -1,5 +1,5 @@
 '''
-MVP demo ver 0.0.1
+MVP demo ver 0.0.2
 2024.10.31
 golf_data/admin.py
 
@@ -14,9 +14,19 @@ class ExcelFileUploadAdmin(admin.ModelAdmin):
     list_display = ('file', 'uploaded_at')
 
     def save_model(self, request, obj, form, change):
+        # 새로운 파일 업로드 전에 기존 파일 삭제
+        ExcelFileUpload.objects.all().delete()
+
+        # 새로 업로드된 파일 저장
         super().save_model(request, obj, form, change)
-        # S3 URL을 import_excel_data 함수에 전달
+
+        # S3 URL을 import_excel_data 함수에 전달하여 데이터 처리
         import_excel_data(obj.file.url)
+
+    def delete_model(self, request, obj):
+        """파일 삭제 시 관련 데이터도 함께 삭제하도록 구현"""
+        obj.file.delete(save=False)  # S3에 저장된 파일 삭제
+        super().delete_model(request, obj)
 
 admin.site.register(GolfClub)
 admin.site.register(GolfCourse)
