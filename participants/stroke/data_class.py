cff864e820b9494b1584e6bcba2381289a831fb7
loading1031 <tjdans1031@gmail.com>
2025-04-12 03:42:23 +0900

    Refactor: redis_client, 참가자 캐싱,  참가자 카운팅 - redis_client 옵션 변경(시간 제한, str 변환 응답) - 처음 조회된 참가자는 mysql에서 읽어서 redis에 저장 - 이후, 모든 수정은 redis에서 진행 - 참가자 수를 카운팅 하여 모두 나가면, redis<->mysql이 동기화되는 로직

--- participants/stroke/data_class.py
+++ participants/stroke/data_class.py
@@ -9,6 +9,7 @@ participa/stroke/data_class.py
 from dataclasses import dataclass
 from typing import Optional
 
+from participants.models import Participant
 
 @dataclass
 class ParticipantUpdateData:
@@ -20,35 +21,18 @@ class ParticipantUpdateData:
     is_group_win_handicap: Optional[bool]
 
     def __post_init__(self):
-        # rank와 handicap_rank가 bytes 타입인 경우 문자열로 디코딩
-        if isinstance(self.rank, bytes):
-            self.rank = self.rank.decode('utf-8')
-        if isinstance(self.handicap_rank, bytes):
-            self.handicap_rank = self.handicap_rank.decode('utf-8')
-        # is_group_win과 is_group_win_handicap이 bytes 타입인 경우 boolean으로 변환
-        if isinstance(self.is_group_win, bytes):
+        if isinstance(self.is_group_win, str):
             self.is_group_win = bool(int(self.is_group_win))
-        if isinstance(self.is_group_win_handicap, bytes):
+        if isinstance(self.is_group_win_handicap, str):
             self.is_group_win_handicap = bool(int(self.is_group_win_handicap))
 
 
 @dataclass
 class EventData:
-    group_win_team: Optional[str]           # 그룹 승리 팀
-    group_win_team_handicap: Optional[str]  # 핸디캡 적용 그룹 승리 팀
-    total_win_team: Optional[str]           # 전체 승리 팀
-    total_win_team_handicap: Optional[str]  # 핸디캡 적용 전체 승리 팀
-
-    def __post_init__(self):
-        # 필드들이 bytes 타입인 경우 문자열로 디코딩
-        if isinstance(self.group_win_team, bytes):
-            self.group_win_team = self.group_win_team.decode('utf-8')
-        if isinstance(self.group_win_team_handicap, bytes):
-            self.group_win_team_handicap = self.group_win_team_handicap.decode('utf-8')
-        if isinstance(self.total_win_team, bytes):
-            self.total_win_team = self.total_win_team.decode('utf-8')
-        if isinstance(self.total_win_team_handicap, bytes):
-            self.total_win_team_handicap = self.total_win_team_handicap.decode('utf-8')
+    group_win_team: str = "NONE"           # 그룹 승리 팀
+    group_win_team_handicap: str = "NONE"  # 핸디캡 적용 그룹 승리 팀
+    total_win_team: str = "NONE"           # 전체 승리 팀
+    total_win_team_handicap: str = "NONE"  # 핸디캡 적용 전체 승리 팀
 
 @dataclass
 class RankResponseData:
@@ -61,87 +45,70 @@ class RankResponseData:
     handicap_score: int
 
     def __post_init__(self):
-        if isinstance(self.participant_id, bytes):
-            self.participant_id = int(self.participant_id)
-        if isinstance(self.last_hole_number, bytes):
-            self.last_hole_number = int(self.last_hole_number)
-        if isinstance(self.last_score, bytes):
-            self.last_score = int(self.last_score)
-        if isinstance(self.rank, bytes):
-            self.rank = self.rank.decode('utf-8')
-        if isinstance(self.handicap_rank, bytes):
-            self.handicap_rank = self.handicap_rank.decode('utf-8')
-        if isinstance(self.sum_score, bytes):
-            self.sum_score = int(self.sum_score)
-        if isinstance(self.handicap_score, bytes):
-            self.handicap_score = int(self.handicap_score)
+        for attr in ['participant_id', 'last_hole_number', 'last_score', 'sum_score', 'handicap_score']:
+            value = getattr(self, attr)
+            if isinstance(value, str) and value.isdigit():
+                setattr(self, attr, int(value))
 
 @dataclass
-class ParticipantResponseData:
+class ParticipantRedisData:
     participant_id: int
-    user_name: chr
-    group_type: chr
-    team_type: chr
-    is_group_win: Optional[bool]
-    is_group_win_handicap: Optional[bool]
-    hole_number: int = 0
-    score: int = 0
-    sum_score: int = 0
-    handicap_score: int = 0
+    event_id: int
+    user_name: str
+    user_handicap: int
+    group_type: str
+    team_type: str
+    sum_score: int 
+    handicap_score: int
+    profile_image: Optional[str] = None
+    is_group_win: bool = False
+    is_group_win_handicap: bool = False
     rank: str = 'N/A'
     handicap_rank: str = 'N/A'
 
     def __post_init__(self):
         # 필드들이 bytes 타입인 경우 적절한 타입으로 변환
-        if isinstance(self.participant_id, bytes):
-            self.participant_id = int(self.participant_id)
-        if isinstance(self.user_name, bytes):
-            self.user_name = self.user_name.decode('utf-8')
-        if isinstance(self.group_type, bytes):
-            self.group_type = self.group_type.decode('utf-8')
-        if isinstance(self.team_type, bytes):
-            self.team_type = self.team_type.decode('utf-8')
-        if isinstance(self.rank, bytes):
-            self.rank = self.rank.decode('utf-8')
-        if isinstance(self.handicap_rank, bytes):
-            self.handicap_rank = self.handicap_rank.decode('utf-8')
-        if isinstance(self.sum_score, bytes):
-            self.sum_score = int(self.sum_score)
-        if isinstance(self.handicap_score, bytes):
-            self.handicap_score = int(self.handicap_score)
-        if isinstance(self.is_group_win, bytes):
-            self.is_group_win = bool(int(self.is_group_win))
-        if isinstance(self.is_group_win_handicap, bytes):
-            self.is_group_win_handicap = bool(int(self.is_group_win_handicap))
-        if isinstance(self.hole_number, bytes):
-            self.hole_number = int(self.hole_number)
-        if isinstance(self.score, bytes):
-            self.score = int(self.score)
+        for attr in ['participant_id', 'event_id', 'user_handicap', 'sum_score', 'handicap_score']:
+            value = getattr(self, attr)
+            if isinstance(value, str) and value.isdigit():
+                setattr(self, attr, int(value))
 
+        for attr in ['is_group_win', 'is_group_win_handicap']:
+            value = getattr(self, attr)
+            if isinstance(value, str) and value.isdigit():
+                setattr(self, attr, bool(int(value)))
 
-@dataclass
-class ParticipantRedisData:
-    participant_id: int
-    group_type: chr
-    team_type: chr
-    is_group_win: Optional[bool]
-    is_group_win_handicap: Optional[bool]
-    sum_score: int
-    handicap_score: int
-
-    def __post_init__(self):
-        # 필드들이 bytes 타입인 경우 적절한 타입으로 변환
-        if isinstance(self.participant_id, bytes):
-            self.participant_id = int(self.participant_id)
-        if isinstance(self.group_type, bytes):
-            self.group_type = self.group_type.decode('utf-8')
-        if isinstance(self.team_type, bytes):
-            self.team_type = self.team_type.decode('utf-8')
-        if isinstance(self.is_group_win, bytes):
-            self.is_group_win = bool(int(self.is_group_win))
-        if isinstance(self.is_group_win_handicap, bytes):
-            self.is_group_win_handicap = bool(int(self.is_group_win_handicap))
-        if isinstance(self.sum_score, bytes):
-            self.sum_score = int(self.sum_score)
-        if isinstance(self.handicap_score, bytes):
-            self.handicap_score = int(self.handicap_score)
+    @staticmethod
+    def orm_to_participant_redis(participant: Participant) -> "ParticipantRedisData":
+        return ParticipantRedisData(
+            participant_id=participant.pk,
+            profile_image=participant.club_member.user.profile_image.url if participant.club_member.user.profile_image else None,
+            event_id=participant.event.pk,
+            group_type=participant.group_type,
+            team_type=participant.team_type,
+            user_name=participant.club_member.user.name,
+            user_handicap=participant.club_member.user.handicap,
+            sum_score=participant.sum_score,
+            handicap_score=participant.handicap_score,
+            is_group_win=participant.is_group_win,
+            is_group_win_handicap=participant.is_group_win_handicap,
+            rank=participant.rank,
+            handicap_rank=participant.handicap_rank
+        )
+    
+    def to_redis_dict(self) -> dict[str, str]:
+        return {
+            "participant_id": str(self.participant_id),
+            'profile_image': self.profile_image if self.profile_image else '',
+            "event_id": str(self.event_id),
+            "user_name": self.user_name,
+            "user_handicap": str(self.user_handicap),
+            "group_type": self.group_type,
+            "team_type": self.team_type,
+            "sum_score": str(self.sum_score),
+            "handicap_score": str(self.handicap_score),
+            "is_group_win": "1" if self.is_group_win else "0",
+            "is_group_win_handicap": "1" if self.is_group_win_handicap else "0",
+            "rank": self.rank,
+            "handicap_rank": self.handicap_rank,
+        }
--- participants/stroke/stroke_event_consumers.py
+++ participants/stroke/stroke_event_consumers.py
@@ -9,11 +9,13 @@ import json
 import logging
 import asyncio
 from dataclasses import asdict
+from typing import List
 
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-from participants.stroke.data_class import RankResponseData
+from participants.models import Participant
+from participants.stroke.data_class import ParticipantRedisData, RankResponseData
 from participants.stroke.mysql_interface import MySQLInterface
 from participants.stroke.redis_interface import redis_client, RedisInterface
 
@@ -40,7 +42,15 @@ class EventParticipantConsumer(AsyncWebsocketConsumer, MySQLInterface, RedisInte
             self.participant_id = self.scope['url_route']['kwargs']['participant_id']
             logging.debug(f'Participant ID: {self.participant_id}')
 
-            participant = await self.get_and_check_participant(self.participant_id, user)
+            # participant = await self.get_and_check_participant(self.participant_id, user) # mysql 연결
+            participant: ParticipantRedisData = await self.get_participant_from_redis(self.event_id,self.participant_id) # redis에서 참가자 정보 가져오기
+            if participant is None:
+                participant_mysql: Participant = await self.get_and_check_participant(self.participant_id, user) # mysql 연결
+                if participant_mysql is None:
+                    participant = None
+                else:
+                    participant = await self.save_participant_in_redis(participant_mysql)  # ✅ 캐싱 추가
+            
             if participant is None:
                 logging.info('No participant')
                 await self.close(code=4004)
@@ -99,7 +109,7 @@ class EventParticipantConsumer(AsyncWebsocketConsumer, MySQLInterface, RedisInte
         try:
             event_data = await self.get_event_data_from_redis(self.event_id)
             # 모든 참가자와 관련된 정보를 한 번의 쿼리로 가져옴
-            participants = await self.get_event_participants(self.event_id)
+            participants: List[ParticipantRedisData] = await self.get_event_participants_from_redis(self.event_id)
             logging.info('participants: {}'.format(participants))
 
             ranks = await asyncio.gather(*[
@@ -123,36 +133,28 @@ class EventParticipantConsumer(AsyncWebsocketConsumer, MySQLInterface, RedisInte
         except Exception as e:
             await self.send_json({'error': f'스코어 기록을 가져오는 데 실패했습니다.{e}'})
 
-    async def process_participant(self, participant):
+    async def process_participant(self, participant: ParticipantRedisData):
         # 참가자 데이터를 처리하여 rank 정보를 반환
 
-        participant_id = participant.id
-        rank_data = await self.get_event_rank_from_redis(participant.event_id, participant_id)
+        rank_data = await self.get_event_rank_from_redis(participant.event_id, participant)
 
         # 아직 점수입력이 안된 참가자는 제외
         if rank_data.sum_score == 0:
             return None
 
-        user = participant.club_member.user
-
         return {
             'user': {
-                'name': user.name,
-                'profile_image' : user.profile_image.url if user.profile_image else None,
+                'name': participant.user_name,
+                'profile_image' : participant.profile_image,
             },
             **asdict(rank_data)  # RankData 객체를 딕셔너리로 변환 후 펼침
         }
 
-    async def get_event_rank_from_redis(self, event_id, participant_id):
+    async def get_event_rank_from_redis(self, event_id, participant: ParticipantRedisData):
         # Redis에서 참가자의 랭킹 정보를 가져옴
 
         logging.info('Fetching hole scores from Redis')
-        redis_key = f'event:{event_id}:participant:{participant_id}'
-
-        rank = await sync_to_async(redis_client.hget)(redis_key, "rank")
-        handicap_rank = await sync_to_async(redis_client.hget)(redis_key, "handicap_rank")
-        sum_score = await sync_to_async(redis_client.hget)(redis_key, "sum_score")
-        handicap_score = await sync_to_async(redis_client.hget)(redis_key, "handicap_score")
+        participant_id = participant.participant_id
 
         # 홀 번호와 점수를 저장하기 위한 초기 변수 설정
         last_hole_number = 0
@@ -164,21 +166,21 @@ class EventParticipantConsumer(AsyncWebsocketConsumer, MySQLInterface, RedisInte
 
         if keys:
             # keys를 내림차순으로 정렬
-            keys.sort(reverse=True, key=lambda k: int(k.decode('utf-8').split(':')[-1]))
+            keys.sort(reverse=True, key=lambda k: int(k.split(':')[-1]))
 
             # 가장 큰 hole_number와 그에 해당하는 score를 가져옴
             last_key = keys[0]
-            last_hole_number = int(last_key.decode('utf-8').split(':')[-1])
+            last_hole_number = int(last_key.split(':')[-1])
             last_score = int(await sync_to_async(redis_client.get)(last_key))
 
         return RankResponseData(
             participant_id=participant_id,
             last_hole_number=last_hole_number,
             last_score=last_score,
-            rank=rank.decode('utf-8') if rank else None,
-            handicap_rank=handicap_rank.decode('utf-8') if handicap_rank else None,
-            sum_score=int(sum_score) if sum_score else 0,
-            handicap_score=int(handicap_score) if handicap_score else 0
+            rank=participant.rank,
+            handicap_rank=participant.handicap_rank,
+            sum_score=participant.sum_score,
+            handicap_score=participant.handicap_score,
         )
 
     async def send_ranks_periodically(self):
