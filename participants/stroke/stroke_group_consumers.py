be454eaed318a865e6e36cafe98a0c6939926960
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-04-30 03:26:24 +0900

    feat: 그룹별 홀 확인/수정 기능 및 상태 관리 로직 구현 - 확인/수정 요청에 따라 다른 로직을 수행할 필요가 있었습니다. - 수정 요청: uncheck_hole, 확인 요창: confirm_hole, 입력: post로 분기처리했습니다. - 확인/수정 부분에서 홀 상태 여부를 관리합니다.

--- participants/stroke/stroke_group_consumers.py
+++ participants/stroke/stroke_group_consumers.py
@@ -70,57 +70,91 @@ class GroupParticipantConsumer(AsyncWebsocketConsumer, RedisInterface, MySQLInte
             await self.close_with_status(500, str(e))
 
     async def receive(self, text_data=None, bytes_data=None, **kwargs):
+        # 각 사람의 스코어 저장 및 각 참가자 랭킹/총합 저장
+
+        # 로직 실행
         try:
             text_data_json = json.loads(text_data)
+            action = text_data_json.get("action")
+            hole_number = text_data_json.get("hole_number")
+            participant_id = text_data_json.get("participant_id")
 
-            if text_data_json['action'] == 'get':
+            # 수정 요청
+            if action == 'uncheck_hole':
+                await self.set_hole_check(self.event_id, self.group_type, hole_number, False)
                 await self.send_scores()
                 return
 
-            participant_id = text_data_json['participant_id'] # request body로 받는 참가자id
-            hole_number = text_data_json['hole_number']
-            score = text_data_json['score']
-            
-            participant: ParticipantRedisData = await self.get_participant_from_redis(event_id=self.event_id,participant_id=participant_id) # redis에서 참가자 정보 가져오기
-            if(participant is None):
-                # 이미 저장된 참가자가 아닐 경우에만, 조회해서 캐싱
-                participant_mysql: Participant = await self.get_participant(participant_id)
-                if participant_mysql is None:
-                    participant = None
-                else:
-                    participant = await self.save_participant_in_redis(participant_mysql)  # ✅ 캐싱 추가
+            # 확인(계산) 요청
+            elif action == 'confirm_hole':
+                participant = await self.get_participant_from_redis(self.event_id, participant_id)
+                if not participant:
+                    await self.send_json({'status': 404, 'error': f'Participant {participant_id} not found'})
+                    return
 
-            if not participant:
-                await self.send_json(self.handle_404_not_found('Participant', participant_id))
+                await self.update_participant_sum_and_handicap_score_in_redis(participant)
+                await self.update_rankings_in_redis(self.event_id)
+
+                logging.info(f'isTeam? {participant.team_type != Participant.TeamType.NONE}')
+                if participant.team_type != Participant.TeamType.NONE:
+                    # 조별 승리 여부 갱신
+                    await self.update_is_group_win_in_redis(participant)
+                    # 전체 이벤트 승리 팀 갱신
+                    await self.update_event_win_team_in_redis(self.event_id)
+
+                # 확인 상태 저장
+                await self.set_hole_check(self.event_id, self.group_type, hole_number, True)
+
+                # 전체 데이터 전송
+                await self.send_scores()
                 return
 
-            if hole_number is None:
-                await self.send_json({'status': 400, 'error': "Hole Number is required."})
+            # 전체 스코어+상태 조회 요청
+            elif action == 'get':
+                await self.send_scores()
                 return
 
-            await self.update_hole_score_in_redis(participant_id, hole_number, score)
-            await self.update_participant_sum_and_handicap_score_in_redis(participant)
-            await self.update_rankings_in_redis(self.event_id)
+            # 스코어 입력
+            elif action == 'post': # TODO: input_score or update_score
+                participant_id = text_data_json['participant_id']  # request body로 받는 참가자id
+                hole_number = text_data_json['hole_number']
+                score = text_data_json['score']
 
-            logging.info(f'isTeam? {participant.team_type != Participant.TeamType.NONE}')
-            if participant.team_type != Participant.TeamType.NONE:
-                logging.info(f'참가자는 팀에 있습니다.')
-                # 조별 승리 여부 갱신
-                await self.update_is_group_win_in_redis(participant)
-                # 전체 이벤트 승리 팀 갱신
-                await self.update_event_win_team_in_redis(participant.event_id)
+                participant: ParticipantRedisData = await self.get_participant_from_redis(event_id=self.event_id,
+                                                                                          participant_id=participant_id)  # redis에서 참가자 정보 가져오기
+                if (participant is None):
+                    # 이미 저장된 참가자가 아닐 경우에만, 조회해서 캐싱
+                    participant_mysql: Participant = await self.get_participant(participant_id)
+                    if participant_mysql is None:
+                        participant = None
+                    else:
+                        participant = await self.save_participant_in_redis(participant_mysql)  # ✅ 캐싱 추가
 
-            participant = await self.get_participant_from_redis(event_id=self.event_id,participant_id=participant_id) # redis에서 갱신된 참가자 정보 가져오기
-            response_data_dict = asdict(participant)
-            response_data_dict["hole_number"] = hole_number
-            response_data_dict["score"] = score
+                if not participant:
+                    await self.send_json(self.handle_404_not_found('Participant', participant_id))
+                    return
 
-            await self.channel_layer.group_send(self.group_name, {
-                'type': 'input_score',
-                **response_data_dict  # Send all response data
-            })
+                if hole_number is None:
+                    await self.send_json({'status': 400, 'error': "Hole Number is required."})
+                    return
 
-            await self.save_celery_event_from_redis_to_mysql(self.event_id, is_count_incr=False) # count 증가 없이, 자동 저장 시간 연장
+                await self.update_hole_score_in_redis(participant_id, hole_number, score)
+
+                participant = await self.get_participant_from_redis(event_id=self.event_id,
+                                                                    participant_id=participant_id)  # redis에서 갱신된 참가자 정보 가져오기
+                response_data_dict = asdict(participant)
+                response_data_dict["hole_number"] = hole_number
+                response_data_dict["score"] = score
+
+                await self.channel_layer.group_send(self.group_name, {
+                    'type': 'input_score',
+                    **response_data_dict  # Send all response data
+                })
+
+                await self.save_celery_event_from_redis_to_mysql(self.event_id,
+                                                                 is_count_incr=False)  # count 증가 없이, 자동 저장 시간 연장
+
+                return
 
         except ValueError as e:
             await self.close_with_status(500, str(e))
@@ -148,7 +182,7 @@ class GroupParticipantConsumer(AsyncWebsocketConsumer, RedisInterface, MySQLInte
     async def send_scores(self):
         try:
             # 그룹에 속한 모든 참가자를 한 번의 쿼리로 가져옴
-            participants = await self.get_group_participants_from_redis(self.event_id,self.group_type)
+            participants = await self.get_group_participants_from_redis(self.event_id, self.group_type)
             logging.info(f'participants: {participants}')
             # 각 참가자의 홀 스코어를 비동기로 병렬 처리
             group_scores = await asyncio.gather(*[
@@ -156,10 +190,15 @@ class GroupParticipantConsumer(AsyncWebsocketConsumer, RedisInterface, MySQLInte
             ])
             logging.info(f'group_scores: {group_scores}')
 
-            await self.send_json(group_scores)
+            hole_checks = await self.get_hole_checks(self.event_id, self.group_type)
+            logging.info(f"hole_checks: {hole_checks}")
+
+            await self.send_json({
+                'scores': group_scores,
+                'hole_checks': hole_checks,
+            })
         except Exception as e:
             await self.send_with_status(500, f'스코어 기록을 가져오는 데 실패했습니다, {e}')
-
 
     async def process_participant(self, participant):
         participant_id = participant.participant_id
@@ -172,7 +211,7 @@ class GroupParticipantConsumer(AsyncWebsocketConsumer, RedisInterface, MySQLInte
             'team_type': participant.team_type,
             'is_group_win': participant.is_group_win,
             'is_group_win_handicap': participant.is_group_win_handicap,
-            'sum_score':participant.sum_score,
+            'sum_score': participant.sum_score,
             'handicap_score': participant.handicap_score,
             'scores': hole_scores,
         }
