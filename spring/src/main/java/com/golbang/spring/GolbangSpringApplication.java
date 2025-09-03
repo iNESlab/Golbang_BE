71a5cbe08f1d70f666d58af815389a5e1eac0af4
zxcvye <yeyeun_134340@naver.com>
2025-09-03 12:06:24 +0900

    Add Spring Boot project initial setup

---
+++ spring/src/main/java/com/golbang/spring/GolbangSpringApplication.java
@@ -0,0 +1,15 @@
+package com.golbang.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolbangSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolbangSpringApplication.class, args);
+	}
+
+}
+
+
